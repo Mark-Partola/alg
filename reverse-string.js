a274a21e99220213cf0af48696f3be434d28cb10
MarkPartola <Partolamark@yandex.ru>
2016-03-29 17:15:15 +0300

    Add second string-reverse

--- reverse-string.js
+++ reverse-string.js
@@ -2,5 +2,9 @@ var strlen = str.length - 1;
 var newstr = new Array(strlen);
 for (var i = 0; i < str.length / 2; i++) {
   newstr[i] = str[strlen - i];
-  newstr[strlen - i] = str[i]
+  newstr[strlen - i] = str[i];
 }
+
+/* -------- */
+
+"Hello".split("").reverse().join("");
